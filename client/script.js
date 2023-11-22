dc08195593af0999e2f7d9c932dbf2f569fcb644
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-22 20:24:57 -0300

    cliente URL

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
  
    loader(messageDiv);
  
-   const response = await fetch('http://localhost:5000', { 
+   const response = await fetch('https://gpt2-q4kz.onrender.com', { 
      method: 'POST',
      headers: {
        'Content-Type': 'application/json',
