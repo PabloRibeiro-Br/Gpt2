f8f8d6210d80df262234c8cdc8abf33e5b81af2e
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-22 21:27:56 -0300

    revolts voltsz

--- client/script.js
+++ client/script.js
@@ -1,63 +1,117 @@
-// server.js
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-const app = express();
-const port = process.env.PORT || 5000;
+let loadInterval
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+function loader(element) {
+   element.textContent = ''
 
-app.use(express.static('public')); // Assuming your HTML/JS files are in a 'public' folder
+   loadInterval = setInterval(() => {
+       // Update the text content of the loading indicator
+       element.textContent += '.';
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/public' });
-});
+       // If the loading indicator has reached three dots, reset it
+       if (element.textContent === '....') {
+           element.textContent = '';
+       }
+   }, 300);
+}
 
-app.post('/', async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
+function typeText(element, text) {
+   let index = 0
 
-    // Placeholder for generateResponse function, replace with actual implementation
-    const response = await generateResponse(prompt);
+   let interval = setInterval(() => {
+       if (index < text.length) {
+           element.innerHTML += text.charAt(index)
+           index++
+       } else {
+           clearInterval(interval)
+       }
+   }, 20)
+}
 
-    if (response.toLowerCase().includes('só respondo sobre transmissões automáticas')) {
-      res.send("Desculpe, só respondo sobre transmissões automáticas.");
-    } else {
-      res.send({
-        bot: response.trim()
-      });
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+   const timestamp = Date.now();
+   const randomNumber = Math.random();
+   const hexadecimalString = randomNumber.toString(16);
 
-// Placeholder for generateResponse function, replace with actual implementation
-const generateResponse = async (prompt) => {
-  // Example: Fetch from an external API
-  const apiResponse = await fetch('https://gpt2-q4kz.onrender.com', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      prompt: prompt,
-    }),
-  });
+   return `id-${timestamp}-${hexadecimalString}`;
+}
 
-  if (apiResponse.ok) {
-    const responseData = await apiResponse.json();
-    return responseData.bot.trim();
-  } else {
-    const err = await apiResponse.text();
-    throw new Error(err);
-  }
-};
+function chatStripe(isAi, value, uniqueId) {
+   return (
+       `
+       <div class="wrapper ${isAi && 'ai'}">
+           <div class="chat">
+               <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+               </div>
+               <div class="message" id=${uniqueId}>${value}</div>
+           </div>
+       </div>
+   `
+   )
+}
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+const handleSubmit = async (e) => {
+   e.preventDefault();
+ 
+   const data = new FormData(form);
+ 
+   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+   form.reset();
+ 
+   const uniqueId = generateUniqueId();
+   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+   chatContainer.scrollTop = chatContainer.scrollHeight;
+
+   const messageDiv = document.getElementById(uniqueId);
+ 
+   loader(messageDiv);
+ 
+   const response = await fetch('https://gpt2-q4kz.onrender.com', { 
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify({
+       prompt: data.get('prompt'),
+     }),
+   });
+ 
+   clearInterval(loadInterval);
+   messageDiv.innerHTML = " ";
+ 
+   if (response.ok) {
+     const responseData = await response.json();
+     const parsedData = responseData.bot.trim();
+ 
+     if (parsedData.toLowerCase().includes('só respondo sobre transmissões automáticas')) {
+       typeText(messageDiv, "Desculpe, só respondo sobre transmissões automáticas.");
+     } else {
+       typeText(messageDiv, parsedData);
+     }
+   } else {
+     const err = await response.text();
+     messageDiv.innerHTML = "Algo deu errado";
+     alert(err);
+   }
+ };
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+   if (e.keyCode === 13) {
+       handleSubmit(e)
+   }
+})
